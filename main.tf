cd40f25a66d34348421b6aeb6d7742e7a3ccceaa
vetrivelpsgcse <34771936+vetrivelpsgcse@users.noreply.github.com>
2026-01-18 15:07:49 +0100

    Add Terraform infrastructure files

---
+++ main.tf
@@ -0,0 +1,13 @@
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "app" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "ai-devops-app"
+  }
+}
